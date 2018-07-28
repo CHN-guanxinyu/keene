1bc14c9058395895cf277213364fef8cdf24e68b
guanxinyu1 <guanxinyu@jd.com>
2018-07-28 21:40:49 +0800

    参数解析器开发完毕

--- src/main/scala/com/keene/core/parsers/KValueTypeArgumentsParser.scala
+++ src/main/scala/com/keene/core/parsers/KValueTypeArgumentsParser.scala
@@ -1,7 +1,5 @@
 package com.keene.core.parsers
 
-import java.lang.reflect.Constructor
-
 
 
 object KValueTypeArgumentsParser {
@@ -39,6 +37,10 @@ object KValueTypeArgumentsParser {
 
 private class KValueTypeArgumentsParser(args: Array[String], _class: Class[_], usage: String) extends ArgumentsParser {
 
+  /**
+    * 解析器主入口
+    * @return
+    */
   def parse: Arguments = {
     stopIfNeedHelp(args)
 
@@ -51,7 +53,7 @@ private class KValueTypeArgumentsParser(args: Array[String], _class: Class[_], u
       val w = k drop 2 split "-"
       val methodName = w.head + w.tail.map(_ capitalize).mkString
 
-      val setter = _constructor.param.namedSetterMapper(methodName)
+      val setter = namedSetterMapper(methodName)
       val plans = Iterator[String => Any](
         _ toInt ,
         _ toDouble ,
@@ -76,45 +78,49 @@ private class KValueTypeArgumentsParser(args: Array[String], _class: Class[_], u
   }
 
 
-  object _constructor {
-    val get: Constructor[_] = _class.getConstructor(param.types: _*)
+  //按照给定`values`设置参数值
+  def newInstance(values: Array[Object]) = constructor.newInstance(values: _*)
 
-
-    object param {
-      lazy val types: Array[Class[_]] =
-        _class.
-          getDeclaredConstructors.
-          head.
-          getParameterTypes
-
-      lazy val namedSetterMapper =
-        $setters.map(s => s.getName.replaceAll("_\\$eq", "") -> s).toMap
-
-      lazy val defaultValues: Array[AnyRef] = $adf.map(_ invoke zero)
-
-      lazy val $setters =
-        $ms.filter(_.getName endsWith "_$eq")
-
-      lazy val $adf =
-        $ms.filter(_.getName startsWith "apply$").
-          sortBy(_.getName.split("\\$").last.toInt)
-
-      lazy val $ms = _class.getDeclaredMethods
-    }
-
-  }
-
-  def newInstance(values: Array[Object]) = _constructor.get.newInstance(values: _*)
-
+  //获取默认参数实体类
   def defaultInstance = {
-    val applyDefaultsParams = _constructor.param.$adf
+    val applyDefaultsParams = $adf
     val z = zero
     val defaultValues = applyDefaultsParams.map(_ invoke z)
     newInstance(defaultValues)
   }
 
+  //参数实体类的唯一构造器
+  lazy val constructor = _class.getConstructor(types: _*)
+
+  //参数实体类构造方法参数类型列表
+  lazy val types: Array[Class[_]] =
+    _class.
+    getDeclaredConstructors.
+    head.
+    getParameterTypes
+
+  //参数实体类成员变量名和对应setter的映射
+  lazy val namedSetterMapper =
+    $setters.map(s => s.getName.replaceAll("_\\$eq", "") -> s).toMap
+
+  //参数实体类默认值列表
+  lazy val defaultValues: Array[AnyRef] = $adf.map(_ invoke zero)
+
+  //参数实体类由scala自动生成的setter方法
+  lazy val $setters =
+    $ms.filter(_.getName endsWith "_$eq")
+
+  //参数实体类由scala自动生成的获取默认参数值的方法
+  lazy val $adf =
+    $ms.filter(_.getName startsWith "apply$").
+      sortBy(_.getName.split("\\$").last.toInt)
+
+  //参数实体类的所有方法
+  lazy val $ms = _class.getDeclaredMethods
+
+  //参数实体类零元
   lazy val zero = {
-    val values = _constructor.param.types.view.
+    val values = types.view.
       map(_.getSimpleName.toLowerCase).
       map {
         case "boolean" => false
@@ -128,6 +134,8 @@ private class KValueTypeArgumentsParser(args: Array[String], _class: Class[_], u
     newInstance(values)
   }
 
+
+  //如果`--help`出现就显示Usage并退出
   def stopIfNeedHelp(org: Array[String]) =
     if (org contains "--help") {
       System.err.println(usage)
