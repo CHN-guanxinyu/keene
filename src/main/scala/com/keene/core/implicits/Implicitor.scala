2ed38700cc9d4cad99e21b591309ef736a0eb8a9
guanxinyu1 <guanxinyu@jd.com>
2018-07-30 17:46:24 +0800

    update files

--- src/main/scala/com/keene/core/implicits/Implicitor.scala
+++ src/main/scala/com/keene/core/implicits/Implicitor.scala
@@ -4,15 +4,11 @@
   * 相比于`fun`(obj)的代码风格我更喜欢`str`.`fun`
   *
   */
-
 package com.keene.core.implicits
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-
-
 trait Implicitor {
-
   implicit def to(sql: String): StringImplicitor = StringImplicitor(sql)
   implicit def to(spark : SparkSession) : SparkSessionImplicitor = SparkSessionImplicitor(spark)
   implicit def to(df : DataFrame) : DataFrameImplicitor = DataFrameImplicitor(df)
