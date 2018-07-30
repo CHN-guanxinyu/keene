f6265a69895dc08026713df25c82c1d0bf2f5020
guanxinyu1 <guanxinyu@jd.com>
2018-07-30 17:46:38 +0800

    update files

--- src/main/scala/com/keene/kafka/KafkaParam.scala
+++ src/main/scala/com/keene/kafka/KafkaParam.scala
@@ -1,14 +1,49 @@
 package com.keene.kafka
 
-case class KafkaParam(
+/**
+  * 默认类型为读取
+  */
+object KafkaParam{
+  def apply(
+    brokers : String,
+    topic : String,
+    as: String = "reader",
+    extraOpts : Map[String,String] = Map.empty
+  ): KafkaParam = as match {
+    case "reader" => KafkaReaderParam(brokers, topic , extraOpts)
+    case "writer" => KafkaWriterParam(brokers, topic , extraOpts)
+    case _ => throw new ClassNotFoundException(s"Kafka${as capitalize}Param")
+  }
+}
+
+sealed abstract class KafkaParam(
+  brokers : String ,
+  extraOpts : Map[String,String]
+) {
+  def opts : Map[String, String]
+  def get : Map[String, String] = Map( "kafka.bootstrap.servers" -> brokers ) ++ opts ++ extraOpts
+}
+
+case class KafkaReaderParam(
+  brokers : String = "",
+  subscribe : String = "",
+  extraOpts : Map[String,String] = Map.empty
+) extends KafkaParam( brokers , extraOpts ){
+
+  override def opts = Map(
+    "subscribe" -> subscribe
+  )
+
+}
+
+case class KafkaWriterParam(
   brokers : String = "",
   topic : String = "",
-  subscribe : String = "",
-  extraOpts : Map[String,String] = Map.empty )
-{
-  def opts: Map[String, String] = Map(
-    "kafka.bootstrap.servers" -> brokers,
-    "subscribe" -> subscribe,
+  extraOpts : Map[String,String] = Map.empty
+) extends KafkaParam( brokers , extraOpts ){
+
+  override def opts = Map(
     "topic" -> topic
-  ).filter(_._2 != "") ++ extraOpts
+  )
+
 }
--- src/main/scala/com/keene/spark/examples/KafkaTest.scala
+++ src/main/scala/com/keene/spark/examples/KafkaTest.scala
@@ -3,19 +3,19 @@ package com.keene.spark.examples
 import com.keene.core.parsers.{Arguments, ArgumentsParser}
 import com.keene.spark.utils.SimpleSpark
 import com.keene.core.implicits._
-import com.keene.kafka.KafkaParam
+import com.keene.kafka.{KafkaParam, KafkaWriterParam}
 
 object KafkaTest extends App with SimpleSpark {
 
   val arg = ArgumentsParser[KafkaReadArgs](args)
 
-  val readParam = KafkaParam( arg.brokers , subscribe = arg.subscribe)
+  val readParam = KafkaParam( arg.brokers , arg.subscribe )
   val kafkaData = spark fromKafka readParam
 
   kafkaData createOrReplaceTempView "t"
 
 
-  val writeParam = KafkaParam( arg.brokers , topic = arg.topic)
+  val writeParam = KafkaWriterParam( arg.brokers , arg.topic )
 
   "select * from t".go toKafka writeParam start
 
