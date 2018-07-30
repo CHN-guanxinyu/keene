00291b13352e583bc8587bf297e08878fd95c8ac
guanxinyu1 <guanxinyu@jd.com>
2018-07-30 11:49:19 +0800

    update kafkatest

--- src/main/scala/com/keene/spark/examples/KafkaReadTest.scala
+++ src/main/scala/com/keene/spark/examples/KafkaReadTest.scala
@@ -1,14 +1,21 @@
 package com.keene.spark.examples
-import com.keene.core.parsers.Arguments
+
+import com.keene.core.parsers.{Arguments, ArgumentsParser}
 import com.keene.spark.utils.SimpleSpark
 
-object KafkaReadTest extends App with SimpleSpark{
-  spark.
-    readStream.
-    format("kafka").
-    options(Map(
-      "kafka.bootstrap.host" -> ""
-    ))
+object KafkaReadTest extends App with SimpleSpark {
+  val argv = ArgumentsParser[KafkaReadArgs](args).parse
+
+  println(argv.host , argv.topics)
+  /*spark.
+  readStream.
+  format("kafka").
+  options(Map(
+    "kafka.bootstrap.host" -> ""
+  ))*/
 }
 
-class KafkaReadArgs() extends Arguments
+class KafkaReadArgs(
+  var host : String = "123",
+  var topics : String = ""
+) extends Arguments
