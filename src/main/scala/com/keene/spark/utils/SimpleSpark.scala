41369f22101700d77a144e2f2d5012127ad5c80d
guanxinyu1 <guanxinyu@jd.com>
2018-07-30 18:15:55 +0800

    update SimpleSpark.scala

--- src/main/scala/com/keene/spark/utils/SimpleSpark.scala
+++ src/main/scala/com/keene/spark/utils/SimpleSpark.scala
@@ -1,44 +1,22 @@
 package com.keene.spark.utils
-import java.util.Properties
-
-import org.apache.spark.sql.{DataFrame, SparkSession => Sss}
+import org.apache.spark.sql.{SparkSession => Sss}
 import org.apache.spark.streaming.{Seconds, StreamingContext => Ssc}
 import org.apache.spark.{SparkContext => Sc}
 
 trait SimpleSpark extends BaseEnv {
-
+  //core
   final lazy val sc = Sc getOrCreate sparkConf
 
+  //sql
   final lazy val spark = {
     val t = Sss.builder
-
     val builder = if (isWindows) t else t.enableHiveSupport
-
     builder config sparkConf getOrCreate
   }
 
+  //streaming
   lazy val ssc: Ssc = Ssc.getActiveOrCreate(() =>
     new Ssc(sparkConf, Seconds(second))
   )
-
-  final def jdbc(
-                  url: String,
-                  table: String,
-                  user: String,
-                  passwd: String
-                ): DataFrame = {
-
-    val prop = new Properties() >>
-      (_ setProperty("user", user)) >>
-      (_ setProperty("password", passwd))
-
-    spark.read jdbc(url, table, prop)
-  }
-
-  implicit final class And[T](obj: T) {
-    def >>(f: T => Unit): T = {
-      f(obj);obj
-    }
-  }
 
 }
