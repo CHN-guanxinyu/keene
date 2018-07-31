e20554663fac7f12694f1f6c4cf9e218895180a5
guanxinyu1 <guanxinyu@jd.com>
2018-07-31 18:00:07 +0800

    update files

---
+++ src/main/scala/com/keene/spark/examples/hive/BaseReadHiveTest.scala
@@ -0,0 +1,39 @@
+package com.keene.spark.examples.hive
+
+import com.keene.core.parsers.{Arguments, ArgumentsParser}
+import com.keene.spark.examples.main.ExampleRunner
+import com.keene.spark.utils.SimpleSpark
+import com.keene.core.implicits._
+
+class BaseReadHiveTest extends SimpleSpark with ExampleRunner{
+  override def run (args: Array[ String ]): Unit = {
+    val arg = ArgumentsParser[BaseReadHiveArg](args)
+
+//    val df = s"select * from ${arg.tableName} limit 100".go
+
+    val ds = spark.readStream.
+      format("socket").
+      option("host" , arg.host).
+      option("port" , arg.port).
+      load
+
+    ds.writeStream.format("console").start
+//    ds.join(df , Seq("key_id"))
+
+    spark.streams.awaitAnyTermination
+  }
+}
+class BaseReadHiveArg(
+  var host : String = "",
+  var port : Int = 0 ,
+  val tableName : String = ""
+) extends Arguments {
+  override def usage =
+    """
+      |Options:
+      |
+      |--host       *
+      |--port       *
+      |--table-name *
+    """.stripMargin
+}
