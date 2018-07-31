2340c686e1d70b9ea48e40ded74761302b769d3e
guanxinyu1 <guanxinyu@jd.com>
2018-07-31 18:02:34 +0800

    update files

---
+++ bin/base_read_hive.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source bin/run_examples.sh
+
+run \
+--klass hive.BaseReadHiveTest \
+--host localhost \
+--port 9999
--- bin/run_examples.sh
+++ bin/run_examples.sh
@@ -4,7 +4,6 @@ function run(){
     spark-submit \
     --master "local[*]" \
     --class "com.keene.spark.examples.main.Main" \
-    --packages "org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.1" \
-    $(ls out/*/*/*.jar) \
     $*
 }
+#--packages "org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.1"
