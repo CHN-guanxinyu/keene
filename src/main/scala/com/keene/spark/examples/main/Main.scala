84f1d57b89aa6406c69e09e1ed6e7e05ebd52b11
guanxinyu1 <guanxinyu@jd.com>
2018-07-31 11:32:52 +0800

    解决代理运行程序的--help问题

--- src/main/scala/com/keene/spark/examples/main/Main.scala
+++ src/main/scala/com/keene/spark/examples/main/Main.scala
@@ -5,16 +5,18 @@ import com.keene.core.implicits._
 
 object Main extends App {
   val arg = Parser[MainArg](args)
-  s"com.keene.spark.examples.${arg.klass}".as[ExampleRunner] run args
+  s"com.keene.spark.examples.${arg.klass}".as[ExampleRunner] run args ++ Seq(if( arg.man ) "--help" else "")
 }
 
 class MainArg(
-  var klass : String = ""
+  var klass : String = "",
+  var man : Boolean = false
 ) extends Arguments {
 
   def usage =
     """Options:
       |--klass
+      |--man        如果需要查看调用类的帮助,将此项设置为true,而不是--help
     """.stripMargin
 
 }
