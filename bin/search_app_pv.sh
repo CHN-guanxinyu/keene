e1d99d833b95926ba4d07bbe99d36de99a071340
guanxinyu1 <guanxinyu@jd.com>
2018-08-01 13:06:38 +0800

    update files

--- bin/search_app_pv.sh
+++ bin/search_app_pv.sh
@@ -4,14 +4,16 @@ source bin/ads_anti.sh
 JAR=$(ls target/scala*/*.jar)
 DATE='2018-07-29'
 
-NUMEXECUTORS=200
+NUM_EXECUTORS=200
 CORES=4
 MEM=16G
 
 run \
---num-executors ${NUMEXECUTORS} \
+--queue "root.bdp_jmart_ad.jd_ad_oozie" \
+--num-executors ${NUM_EXECUTORS} \
 --executor-cores ${CORES} \
 --executor-memory ${MEM} \
+${JAR} \
 --class sync.SearchAppPv \
 --date ${DATE} \
 --result-table antidb.keen_search_app_pv_log
