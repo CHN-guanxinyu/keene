d35f8df48ce017a16133e56b60dff887667e7173
guanxinyu1 <guanxinyu@jd.com>
2018-08-15 14:33:16 +0800

    添加采样器隐式转换

--- src/main/scala/com/keene/core/implicits/SeqImlicitor.scala
+++ src/main/scala/com/keene/core/implicits/SeqImlicitor.scala
@@ -3,7 +3,9 @@ package com.keene.core.implicits
 import scala.util.Random
 
 case class SeqImlicitor(@transient t : Seq[_]) {
-  def sample(n : Int = 20): Seq[_] ={
+  def sample = sample(20)
+
+  def sample(n : Int): Seq[_] ={
     val groupSize = t.size / n
     if(groupSize == 0) t
     else{
