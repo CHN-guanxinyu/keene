bdd0510ca0bf0e17e0d226f3960d2152f42ab807
guanxinyu1 <guanxinyu@jd.com>
2018-08-15 14:10:25 +0800

    添加采样器隐式转换

--- src/main/scala/com/keene/core/implicits/SeqImlicitor.scala
+++ src/main/scala/com/keene/core/implicits/SeqImlicitor.scala
@@ -8,11 +8,15 @@ case class SeqImlicitor(@transient t : Seq[_]) {
     if(groupSize == 0) t
     else{
       val it = t grouped groupSize
-      val (init, last) = ((List[Seq[_]](), it.next() ) /: it) { case ( (init, last), e ) =>
-        (last :: init, e)
-      }
+      val (init, last) = fastInitAndLast(it)
       val rand = new Random
       (init.par.map{ _(rand nextInt groupSize) } :+ last( rand nextInt last.size )) toList
     }
   }
+
+  private def fastInitAndLast[T](it : Iterator[T])=
+    ((List.empty[T], it.next ) /: it) {
+      case ( (init, last), e ) => (last :: init, e)
+    }
+
 }
