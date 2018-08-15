db50b9b4b696d6045835c02038994da828978818
guanxinyu1 <guanxinyu@jd.com>
2018-08-15 17:16:45 +0800

    二叉树重建新解

--- src/main/java/niuke/ReConstructBinaryTree.java
+++ src/main/java/niuke/ReConstructBinaryTree.java
@@ -18,6 +18,7 @@ public class ReConstructBinaryTree {
         if (pre.length == 0) return null;
         if (pre.length == 1) return new TreeNode(pre[0]);
 
+        //初始化
         int len = pre.length;
         TreeNode[] nodeArr = new TreeNode[len];
         Map<Integer,Integer> indices = new HashMap();
@@ -26,20 +27,17 @@ public class ReConstructBinaryTree {
             nodeArr[i] = new TreeNode(in[i]);
         }
 
+        //核心
         int pi, qi;
         for (int p = 0; p < len - 1; p++) {
             pi = indices.get(pre[p]);
             qi = indices.get(pre[p + 1]);
             int t = qi;
             if (pi < qi) {
-                while (
-                        t > pi &&
-                                nodeArr[t].left == null &&
-                                nodeArr[t].right == null
-                        ) t--;
+                TreeNode tn;
+                while ( t > pi && (tn = nodeArr[t]).left == tn.right ) t--;
                 nodeArr[t].right = nodeArr[qi];
             } else nodeArr[pi].left = nodeArr[qi];
-
         }
         return nodeArr[indices.get(pre[0])];
     }
