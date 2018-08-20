6342fb60542697840d3c2c8838e0b7ef50d9e58f
guanxinyu1 <guanxinyu@jd.com>
2018-08-20 10:09:22 +0800

    update files

--- src/main/scala/com/keene/spark/examples/hive/Free.scala
+++ src/main/scala/com/keene/spark/examples/hive/Free.scala
@@ -1,16 +1,16 @@
 package com.keene.spark.examples.hive
 
 import com.keene.core.Runner
-import com.keene.core.parsers.Arguments
+import com.keene.core.parsers.{Arguments, ArgumentsParser}
 import com.keene.spark.utils.SimpleSpark
 
 class Free extends Runner with SimpleSpark {
   override def run (args: Array[ String ]): Unit = {
-    val arg
+    val arg = ArgumentsParser[FreeArg](args)
   }
 }
 
-class FreeArgs(
+class FreeArg(
   var date : String = ""
 ) extends Arguments {
   override def usage =
