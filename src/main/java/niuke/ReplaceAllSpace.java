4509de52c7e67b9d70391075481b94ed23a41ef7
guanxinyu1 <guanxinyu@jd.com>
2018-08-30 11:18:06 +0800

    update files

--- src/main/java/niuke/ReplaceAllSpace.java
+++ src/main/java/niuke/ReplaceAllSpace.java
@@ -12,9 +12,17 @@ import java.util.Deque;
  * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
  * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
  */
-public class ReplaceAllSpace {
+public class ReplaceAllSpace extends A{
+    public ReplaceAllSpace(){
+        System.out.println(3);
+        a = 10;
+    }
+    static {
+        System.out.println(4);
+
+    }
     public static void main(String[] args){
-        System.out.println(new ReplaceAllSpace().replaceSpace(new StringBuffer("Hello World")));
+        new ReplaceAllSpace();
     }
     public String replaceSpace(StringBuffer str) {
 
@@ -27,5 +35,16 @@ public class ReplaceAllSpace {
         }
 
         return sb.toString();
+    }
+}
+class A{
+    static int a;
+    static {
+        System.out.println(1);
+        System.out.println(a);
+    }
+    public A(){
+        System.out.println(2);
+        System.out.println(a);
     }
 }
