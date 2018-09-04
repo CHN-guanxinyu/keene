15fcaf3a73582c19734cadef5f5b9a6949624a4c
guanxinyu1 <guanxinyu@jd.com>
2018-09-04 11:05:19 +0800

    update files

--- src/main/scala/com/keene/spark/examples/kafka/ForTest.scala
+++ src/main/scala/com/keene/spark/examples/kafka/ForTest.scala
@@ -6,7 +6,7 @@ import com.keene.spark.utils.SimpleSpark
 
 class ForTest extends Runner with SimpleSpark{
   override def run(implicit argv: Array[String]): Unit = {
-    spark.fromKafka("kafka-broker2.jd.local:9092", "kafka-broker2.jd.local:9092").toConsole
+    spark.fromKafka("kafka-broker2.jd.local:9092", "dx_anti_search_click_logs").toConsole
     spark.streams.awaitAnyTermination()
   }
 }
